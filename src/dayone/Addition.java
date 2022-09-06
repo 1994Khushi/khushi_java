2c043fe3cb52c1a864012bc3ccc2d30e0bfade6b
1994Khushi <ayangelyoung@gmai.com>
2022-09-06 16:08:44 +0530

    Addition

---
+++ src/dayone/Addition.java
@@ -0,0 +1,24 @@
+package dayone;
+
+public class Addition {
+
+    public static void main(String[] args) {
+
+        int a=20;
+        int b=37;
+        int c=12;
+
+        int d;
+
+        d = a + b + c;
+
+        System.out.println("Result" + d);
+
+    }
+
+
+
+
+
+
+}
